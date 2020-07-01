ff54edb258b7f26713f1efac1a7de3a295598b5d
maureenbarahona <maureenbarahona@gmail.com>
2020-06-30 22:48:54 -0600

    implementacion de clase persona

--- src/main/java/hn/uth/poo/p2/objetos/Persona.java
+++ src/main/java/hn/uth/poo/p2/objetos/Persona.java
@@ -10,5 +10,55 @@ package hn.uth.poo.p2.objetos;
  * @author maureen
  */
 public class Persona {
+    private int codPersona;
+    private String nomPersona;
+    private String direccion;
+    private String telefono;
+
+    public Persona() {
+    }
+    
+    
+
+    public Persona(int codPersona, String nomPersona, String direccion, String telefono) {
+        this.codPersona = codPersona;
+        this.nomPersona = nomPersona;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+    
+    
+
+    public int getCodPersona() {
+        return codPersona;
+    }
+
+    public void setCodPersona(int codPersona) {
+        this.codPersona = codPersona;
+    }
+
+    public String getNomPersona() {
+        return nomPersona;
+    }
+
+    public void setNomPersona(String nomPersona) {
+        this.nomPersona = nomPersona;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
 }
